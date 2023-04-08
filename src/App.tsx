f652ef9a4da49467370df0ee4f309882766fbda9
Jonathankjellen <55298459+Jonathankjellen@users.noreply.github.com>
2023-04-08 17:10:54 +0200

    changed to react app instead of expo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from "./components/Card";
 
 function App() {
+  const [clickedButton, setClickedButton] = useState('');
+  const [betAmount, setBetAmount] = useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Card value="one" suit="heart"/>
+        <Card value="two" suit="clubs"/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { CardProps } from './types';
+import styles from './styles/card.module.css'
+//import "./styles/card.css"
+
+const Card = ({value, suit}: CardProps) => {
+    const getColor =() => {
+        if(suit=="spades" || suit=="clubs"){
+            return styles.black
+        } else {
+            return styles.red
+        }
+    }
+    return (
+        <div className={styles.card} >
+            <div className={getColor()}>
+                <h1 className={`${styles.value}`}>{value}</h1>
+                <h1 className={styles.suit}>{suit}</h1>
+            </div>
+        </div>
+        
+        
+    )
+}
+
+export default Card
